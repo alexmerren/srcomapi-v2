4f4222770e80cfe8a0283b4cfc49c27bc59eaecc
Alex Merren <alexandermerren@gmail.com>
2023-04-21 19:42:42 +0100

    *

--- main.py
+++ main.py
@@ -5,12 +5,13 @@ def format_json(data):
     print(json.dumps(data, indent=4))
 
 def main():
-    game_id = "k6qg0xdg"
-    game_data = api.get_game_data(game_id)
-    categories = [category['id'] for category in game_data['categories']]
-    category_id  = categories[0]
-    leaderboard_data = api.get_game_category_leaderboard(game_id, category_id, page_number=1)
-    format_json(leaderboard_data)
+    while True:
+        game_id = "k6qg0xdg"
+        game_data = api.get_game_data(game_id)
+        categories = [category['id'] for category in game_data['categories']]
+        category_id  = categories[0]
+        data = api.get_game_category_record_history(game_id, category_id)
+        format_json(data)
 
 if __name__ == "__main__":
     main()
--- srcomapiv2/api.py
+++ srcomapiv2/api.py
@@ -11,33 +11,32 @@ __all__ = [
 ]
 
 def get_user_data(user_id, data=None):
-    if data == None: 
+    if data is not None:
         data = {
             "userId": user_id,
             "levelType": 1,
         }
     return requests.request_function_with_data('UserLeaderboard', data)
 
-def get_game_list(data=None):
-    # The data here is useless, it maintains style with the other functions.
+def get_game_list():
     return requests.request_function('GameList')
 
 def get_game_data(game_id, data=None):
-    if data == None: 
+    if data is None:
         data = {
             'gameId': game_id,
         }
     return requests.request_function_with_data('GameData', data)
 
 def get_game_summary(game_url, data=None):
-    if data == None: 
+    if data is None:
         data = {
             "gameUrl": game_url,
         }
     return requests.request_function_with_data('GameSummary', data)
 
 def get_game_category_leaderboard(game_id, category_id, page_number=1, data=None):
-    if data == None:
+    if data is None:
         data = {
             "params": {
                 "gameId": game_id,
@@ -54,7 +53,7 @@ def get_game_category_leaderboard(game_id, category_id, page_number=1, data=None
     return requests.request_function_with_data('GameLeaderboard', data)
 
 def get_game_category_record_history(game_id, category_id, data=None):
-    if data == None:
+    if data is None:
         data = {
             "params": {
                 "gameId": game_id,
@@ -64,9 +63,9 @@ def get_game_category_record_history(game_id, category_id, data=None):
     return requests.request_function_with_data('GameRecordHistory', data)
 
 def get_game_latest_leaderboard(game_id, limit, data=None):
-    if data == None:
+    if data is None:
         data = {
             "gameId": game_id,
-            "limit": limit
+            "limit": limit,
         }
     return requests.request_function_with_data('GameLatestLeaderboard', data)
