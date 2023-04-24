d7d68cc3b00b4a030a7182ef0c6c3de52379b0e4
Alex Merren <67416656+alexmerren@users.noreply.github.com>
2023-04-24 12:00:53 +0100

    Add Version and User-Agent (#3)

--- main.py
+++ main.py
@@ -5,9 +5,19 @@ def format_json(data):
     print(json.dumps(data, indent=4))
 
 def main():
-    user_id = "zxznzp0x"
-    data = api.get_user_data(user_id)
-    format_json(data)
+    data = api.get_game_list()
+    num_pages = data['pagination']['pages']
+    all_games = set()
+    for page_number in range(1, num_pages):
+        data = api.get_game_list(page_number)
+        games = data['games']
+        for game in games:
+            all_games.add(game['id'])
+
+    with open('test.csv', 'w', encoding='utf-8') as openfile:
+        game_string = ','.join(list(all_games)) + '\n'
+        openfile.write(f"Number of games: {len(all_games)}")
+        openfile.write(game_string)
 
 if __name__ == "__main__":
     main()
